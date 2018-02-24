0fc646572ee1deef0c1f2abf9a6f59832c5b8999
Camilo Giraldo <camilogiraldo91@gmail.com>
2018-02-24 09:56:27 -0500

    Css modules enabled

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import classes from './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={classes.App}>
+        <header className="App-header">
+          <img src={logo} className={classes.AppLogo} alt="logo" />
+          <h1 className={classes.AppTitle}>Welcome to React</h1>
+        </header>
+        <p className={classes.AppIntro}>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
